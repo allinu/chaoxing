f4f0a4a5497d4aa36c20f527249d5929e2a73689
Liona Maskros <lionacc@163.com>
2021-03-26 18:54:09 +0800

    :bug: 修补BUG
    更改环境变量的读取类型

--- run.py
+++ run.py
@@ -11,9 +11,9 @@ classid = os.environ.get("CLASSID")
 # courseid = '208436724'
 courseid = os.environ.get("COURSEID")
 # 请求间隔时间,根据需要更改,单位:秒
-sleep_time = os.environ.get("SLEEP_TIME")
+sleep_time = float(os.environ.get("SLEEP_TIME"))
 # 程序总执行时长,单位:分钟
-pass_time = os.environ.get("PASS_TIME")
+pass_time = float(os.environ.get("PASS_TIME"))
 # 下面无需修改
 cx = Chaoxing(cookies, classid, courseid, sleep_time, pass_time)
 cx.run()
