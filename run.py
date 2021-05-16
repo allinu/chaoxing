708e23585bcd007d6321fe758ad77cbb58072786
Liona <lionacc@163.com>
2021-05-16 16:18:49 +0800

    :memo: 更新文件

--- run.py
+++ run.py
@@ -3,16 +3,19 @@ import os
 
 # 你的Cookie信息
 # 具体说明看文档
-cookies = os.environ.get('COOKIE')
+# cookies = ""
+cookies = os.environ.get("COOKIE")
 # ClassID
-# classid = '39007569'
+# classid = "39007569"
 classid = os.environ.get("CLASSID")
 # CourseID
-# courseid = '208436724'
+# courseid = "208436724"
 courseid = os.environ.get("COURSEID")
 # 请求间隔时间,根据需要更改,单位:秒
+# sleep_time = 0.5
 sleep_time = float(os.environ.get("SLEEP_TIME"))
 # 程序总执行时长,单位:分钟
+# pass_time = 10
 pass_time = float(os.environ.get("PASS_TIME"))
 # 下面无需修改
 cx = Chaoxing(cookies, classid, courseid, sleep_time, pass_time)
